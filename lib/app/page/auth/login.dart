ce64217a588d199a16567a25e69c18b42c626dcf
Ngo Nhat Huy <20dh110616@st.huflit.edu.vn>
2024-04-20 17:14:26 +0700

    update login & data product

--- lib/app/page/auth/login.dart
+++ lib/app/page/auth/login.dart
@@ -30,6 +30,7 @@ class _LoginScreenState extends State<LoginScreen> {
 
   login() async {
     //lấy token (lưu share_preference)
+    try{
     token = await APIRepository()
         .login(accountController.text, passwordController.text);
     var user = await APIRepository().current(token);
@@ -49,6 +50,22 @@ class _LoginScreenState extends State<LoginScreen> {
     // Navigator.push(
     //     context, MaterialPageRoute(builder: (context) => const Mainpage()));
     return token;
+    }
+    catch (error){
+      setState(() {
+        isLoading = false;
+      });
+
+       ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Đăng nhập không thành công!\n Vui lòng kiểm tra lại.',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
   }
 
   @override
@@ -61,120 +78,122 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Container(
           alignment: Alignment.center,
           child: Padding(
-            padding: const EdgeInsets.only(
-                bottom: 10.0, left: 10.0, right: 10.0, top: 50.0),
-            child: Center(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    Image.asset(
-                      urlLogo,
-                      errorBuilder: (context, error, stackTrace) =>
-                          const Icon(Icons.image),
-                    ),
-                    const Text(
-                      "LOGIN INFORMATION",
-                      style: TextStyle(fontSize: 24, color: Colors.blue),
-                    ),
-                    TextFormField(
-                      textInputAction: TextInputAction.next,
-                      controller: accountController,
-                      validator:
-                          RequiredValidator(errorText: 'Please enter Account'),
-                      decoration: const InputDecoration(
-                        labelText: "Account",
-                        icon: Icon(Icons.person),
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        urlLogo,
+                        errorBuilder: (context, error, stackTrace) =>
+                            const Icon(Icons.image),
                       ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextFormField(
-                      textInputAction: TextInputAction.go,
-                      validator:
-                          RequiredValidator(errorText: 'Please Enter Password'),
-                      onFieldSubmitted: (value) => login,
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: "Password",
-                        icon: Icon(Icons.password),
+                      const Text(
+                        "THÔNG TIN ĐĂNG NHẬP",
+                        style: TextStyle(fontSize: 24, color: Colors.blue),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                            child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blue,
-                          ),
-                          onPressed: () async {
-                            //kiểm tra nếu pass hết validator
-                            if (_formKey.currentState!.validate()) {
-                              if (isLoading) return;
-                              setState(
-                                () => isLoading = true,
-                              );
-                              // login();
-
-                              //thông báo lỗi nếu ko tìm thấy user
-                              String response = await login();
-                              if (response == token) {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const Mainpage()));
-                                setState(
-                                  () => isLoading = false,
-                                );
-                              } 
-                              else {
-                                showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) {
-                                      return const AlertDialog(
-                                        title: Text("Alert"),
-                                        content: SingleChildScrollView(
-                                          child: Text(
-                                              "Please check your information again!"),
-                                        ),
-                                      );
-                                    });
-                                setState(
-                                  () => isLoading = false,
-                                );
-                              }
-                            }
-                          },
-                          child: isLoading
-                              ? LoadingAnimationWidget.waveDots(
-                                  color: Colors.white, size: 35)
-                              : const Text(
-                                  "Login",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                        )),
-                        const SizedBox(
-                          width: 16,
+                      TextFormField(
+                        textInputAction: TextInputAction.next,
+                        controller: accountController,
+                        validator: RequiredValidator(
+                            errorText: 'Please enter Account'),
+                        decoration: const InputDecoration(
+                          labelText: "Account",
+                          icon: Icon(Icons.person),
                         ),
-                        Expanded(
-                            child: OutlinedButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const Register()));
-                          },
-                          child: const Text("Register"),
-                        ))
-                      ],
-                    )
-                  ],
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        textInputAction: TextInputAction.go,
+                        validator: RequiredValidator(
+                            errorText: 'Please Enter Password'),
+                        onFieldSubmitted: (value) => login,
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: "Password",
+                          icon: Icon(Icons.password),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                            ),
+                            onPressed: () async {
+                              //kiểm tra nếu pass hết validator
+                              if (_formKey.currentState!.validate()) {
+                                if (isLoading) return;
+                                setState(
+                                  () => isLoading = true,
+                                );
+                                // login();
+
+                                //thông báo lỗi nếu ko tìm thấy user
+                                String response = await login();
+                                if (response == token) {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              const Mainpage()));
+                                  setState(
+                                    () => isLoading = false,
+                                  );
+                                } else {
+                                  showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return const AlertDialog(
+                                          title: Text("Alert"),
+                                          content: SingleChildScrollView(
+                                            child: Text(
+                                                "Please check your information again!"),
+                                          ),
+                                        );
+                                      });
+                                  setState(
+                                    () => isLoading = false,
+                                  );
+                                }
+                              }
+                            },
+                            child: isLoading
+                                ? LoadingAnimationWidget.waveDots(
+                                    color: Colors.white, size: 35)
+                                : const Text(
+                                    "Login",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                          )),
+                          const SizedBox(
+                            width: 16,
+                          ),
+                          Expanded(
+                              child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const Register()));
+                            },
+                            child: const Text("Register"),
+                          ))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
         ),
